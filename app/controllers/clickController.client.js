86f0338a77ad8aa8f4c6041221502f5e6b9182c1
Joe Somma <jsomma@millentek.com>
2015-11-20 15:38:12 -0500

    Defined new angular module and click controller with clicks mock to display click count in the view

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -1,53 +1,11 @@
 'use static';
 
 (function () {
-	var addButton = document.querySelector('.btn-add');
-	var deleteButton = document.querySelector('.btn-delete');
-	var clickNbr = document.querySelector('#click-nbr');
-	var apiUrl = 'http://localhost:3000/api/clicks';
+
+   angular
+      .module('clementineApp', [])
+      .controller('clickController', ['$scope', function ($scope) {
+         $scope.clicks = 1000;
+      }]);
 	
-	function ready (fn) {
-		if (typeof fn !== 'function') {
-			return;
-		}
-		
-		if (document.readyState === 'complete') {
-			return fn();
-		}
-		
-		document.addEventListener('DOMContentLoaded', fn, false);
-	}
-	
-	function ajaxRequest (method, url, callback) {
-		var xmlhttp = new XMLHttpRequest();
-		
-		xmlhttp.onreadystatechange = function () {
-			if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-				callback(xmlhttp.response);
-			}
-		};
-		
-		xmlhttp.open(method, url, true);
-		xmlhttp.send();
-	}
-	
-	function updateClickCount (data) {
-		var clicksObject = JSON.parse(data);
-		clickNbr.innerHTML = clicksObject.clicks;
-	}
-	
-	// This is essentiall your onLoad function that is firing first in this controller
-	ready(ajaxRequest('GET', apiUrl, updateClickCount));
-	
-	addButton.addEventListener('click', function () {
-		ajaxRequest('POST', apiUrl, function () {
-			ajaxRequest('GET', apiUrl, updateClickCount)
-		});
-	}, false);
-	
-	deleteButton.addEventListener('click', function () {
-		ajaxRequest('DELETE', apiUrl, function () {
-			ajaxRequest('GET', apiUrl, updateClickCount);
-		});
-	}, false);
 })();
