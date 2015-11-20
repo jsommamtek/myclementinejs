c7812317743e9e0e9cd489178d3a85dcad7f0e0e
Joe Somma <jsomma@millentek.com>
2015-11-20 15:53:45 -0500

    Finished adding add and reset mocks to new angular controller

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -5,7 +5,16 @@
    angular
       .module('clementineApp', [])
       .controller('clickController', ['$scope', function ($scope) {
-         $scope.clicks = 1000;
-      }]);
-	
+         
+         $scope.clicks = 0;
+         
+         $scope.addClick = function () {
+           $scope.clicks += 1;
+         };
+         
+         $scope.resetClicks = function () {
+           $scope.clicks = 0; 
+         };
+         
+      }]);	
 })();
