9d5adb9206c98ab8b38a8791a54579380666c8e8
Joe Somma <jsomma@millentek.com>
2015-11-23 16:04:55 -0500

    Updating port for node server that matches heroku

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ mongo.connect(process.env.MONGOLAB_URI, function (err, db) {
 	});
 	*/
 
-   app.listen(3000, function () {
-      console.log('Listening on port 3000...');
+   app.listen(5000, function () {
+      console.log('Listening on port 5000...');
    });
 
 });
